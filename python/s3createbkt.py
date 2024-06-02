aae0f927d733bb4ff708a245314694272d4646a2
2gauravc <2gauravc@gmail.com>
2024-06-02 02:44:24 +0000

    updated py s3 code

--- python/s3createbkt.py
+++ python/s3createbkt.py
@@ -6,17 +6,23 @@ session = boto3.session.Session(profile_name='default')
 
 # Retrieve region from session object
 current_region = session.region_name
+bkt_create_region = 'us-east-2'
+
+bkt_name = 'demobkt1977-12'
 print ("Current region is: ", current_region)
+print ("Creating bucket in region : ", bkt_create_region)
+
+
 # Create a high-level resource from custom session
 resource = session.resource('s3')
-bucket = resource.Bucket('demobkt1977-1')
+bucket = resource.Bucket(bkt_name)
 # Region-specific endpoints require the LocationConstraint parameter
 bucket.create(
-    CreateBucketConfiguration={'LocationConstraint': current_region})
+    CreateBucketConfiguration={'LocationConstraint': bkt_create_region})
 
 # Create a waiter that counts the number of seconds till the bucket is created 
 t0 = time.time()
-waiter = bucket.wait_until_exists('demobkt1977-1')
+waiter = bucket.wait_until_exists()
 t1 = time.time()
 total = t1-t0
 
--- python/s3headbkt.py
+++ python/s3headbkt.py
@@ -2,29 +2,34 @@ import boto3, botocore
 
 # declare a function to vrify the bucket 
 def verifyBucketName(s3Client,bucket):
+    print("Checking for bucket: {}".format(bucket))
     try:
         ## check if a bucket already exists in AWS
         s3Client.head_bucket(Bucket=bucket)
         # If the previous command is successful, the bucket is already in your account.
-        raise SystemExit('This bucket has already been created')
+        raise SystemExit('Bucket exists and is owned by this account.')
     except botocore.exceptions.ClientError as e:
         error_code=int(e.response['Error']['Code'])
         if error_code == 404:
             ## If you receive a 404 error code, a bucket with that name
             ##  does not exist anywhere in AWS.
-            print('Existing bucket Not Found, please proceed')
+            print('Error Code 404. No such bucket exists.')
         if error_code == 403:
             ## If you receive a 403 error code, a bucket with that name exists 
             ##  in another AWS account.
-            raise SystemExit('This bucket is already owned by another AWS Account')
-        
+            raise SystemExit('Error Code 403. Permission errors in accessing bucket. Likely bucket owned by another account.')
+        if error_code == 400:
+            ## If you receive a 400 error code, a bucket with that name exists 
+            ##  in another AWS region.
+            raise SystemExit('Error Code 400. Bucket exists not in this region.')
 # Create S3 resource using custom session
 session = boto3.session.Session(profile_name='default')
 
 # Retrieve region from session object
 current_region = session.region_name
+print("Current region is: {}".format(current_region))
 
 # Create a high-level resource from custom session
 client = session.client('s3')
 
-verifyBucketName(client, 'demo--bkt-serverless')
+verifyBucketName(client, 'demobkt1977-12')
